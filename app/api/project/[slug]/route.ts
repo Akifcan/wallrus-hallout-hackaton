4d22c61facd71b2c1c36e8c438832e28b5849a39
Akifcan <akfkara97@gmail.com>
2025-11-23 22:55:59 +0300

    added project slug

--- app/api/project/[slug]/route.ts
+++ app/api/project/[slug]/route.ts
@@ -3,12 +3,14 @@ import { NextResponse } from "next/server";
 
 export async function GET(
   request: Request,
-  { params }: { params: { slug: string } }
+  { params }: { params: Promise<{ slug: string }> }
 ) {
+  const { slug } = await params;
+
   const project = await supabase
     .from("project")
     .select("*")
-    .eq("slug", params.slug)
+    .eq("slug", slug)
     .single();
 
   if (!project.data) {
